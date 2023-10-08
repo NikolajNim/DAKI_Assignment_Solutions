080a9269f88b8455a73ef451864946fdb3f776ab
Nikolaj Nim <nnjo22@student.aau.dk>
2023-10-08 15:42:54 +0200

    AnalogClock done

--- Miniprojekt 1 Analog Clock/AnalogClock.py
+++ Miniprojekt 1 Analog Clock/AnalogClock.py
@@ -25,16 +25,6 @@ def main():
 
     generate_watch_face(angle_step, angle_step_min, black, center, endP_list, endP_min_list, radius, screen, white)
 
-    font = pg.font.Font(None, 100)
-    for i in range(1, 13):
-        rad_angle = math.radians(i * angle_step)
-        x = center[0] + radius + 20 * math.cos(rad_angle) - 15
-        y = center[1] - radius + 20 * math.sin(rad_angle) - 20
-        number_text = font.render(str(i), True, black)
-        text_rect = number_text.get_rect()
-        text_rect.center(x, y)
-        screen.blit(number_text, text_rect)
-
     run_flag = True
     while run_flag is True:
 
@@ -42,10 +32,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run_flag = False
+        pg.display.set_caption("Nikolaj Nims Analog Ur")
         pg.display.flip()  # Refresh the screen so drawing appears
 
-#This function genereates the watchface using most of the initializes variables.
+#This function genereates the watchface using most of the initialized variables.
 def generate_watch_face(angle_step, angle_step_min, black, center, endP_list, endP_min_list, radius, screen, white):
+    #This loop generates the minutlines on the watch face
     for i in range(60):
         angle_radian = math.radians(i * angle_step_min)
         end_x = center[0] + radius * math.cos(angle_radian)
@@ -55,6 +47,7 @@ def generate_watch_face(angle_step, angle_step_min, black, center, endP_list, en
 
     pg.draw.circle(screen, white, center, radius - 20)
 
+    #This loop generates the hourlines on the watch face
     for i in range(12):
         angle_radian = math.radians(i * angle_step)
         end_x = center[0] + radius * math.cos(angle_radian)
@@ -62,9 +55,8 @@ def generate_watch_face(angle_step, angle_step_min, black, center, endP_list, en
         endP_list.append((end_x, end_y))
         pg.draw.line(screen, black, center, (int(end_x), int(end_y)), 5)
 
-    pg.draw.circle(screen, white, center, radius - 50)
     pg.draw.circle(screen, black, center, radius, 4)
-    pg.draw.circle(screen, black, center, 4)
+
 
 #This function is used to calculate the next position of a "watch hand"
 #This is only used for watch hands, because of the needed time parameter
@@ -77,16 +69,25 @@ def cal_pos(center, radius, time, angle):
 
 #This function is suppose to run in the while, because it moves the watchhands by implementing time
 def watch(screen, white, center, radius, red, blue, green):
+    #This white circle is responsible for wiping the watch face clean
+    #so the animation of the hands moving is possible, each time this watch() fanction is called
     pg.draw.circle(screen, white, center, radius +10)
 
+    #Here time is implemted by using datetime.now()
+    #and each variable from microseconds to minute,
+    #have been added to the next time variable to make the watch hands run smooth
     current_time = dt.datetime.now()
     microsec = current_time.microsecond
     sec = current_time.second + (microsec/1000000)
     minute = current_time.minute + (sec/6) * 0.1
     hour = current_time.hour + (minute/6) * 0.1
 
-    pg.draw.line(screen, red, center, cal_pos(center, radius, sec, 6), 2)
-    pg.draw.line(screen, blue, center, cal_pos(center, radius, minute, 6), 3)
+    #Here are the watch hands drawn
     pg.draw.line(screen, green, center, cal_pos(center, radius - 40, hour, 30), 4)
+    pg.draw.line(screen, blue, center, cal_pos(center, radius, minute, 6), 3)
+    pg.draw.line(screen, red, center, cal_pos(center, radius, sec, 6), 2)
+
+
+    pg.draw.circle(screen, (0, 0, 0), center, 4)
 
 main()
