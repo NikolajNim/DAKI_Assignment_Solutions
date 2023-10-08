0a88e500e41504fb1d1cb334bb16a4c8593e40e1
Nikolaj Nim <nnjo22@student.aau.dk>
2023-10-08 20:04:58 +0200

    AnalogClock done

--- Lection1/lesson6UserInputWhileLoopBookExercises.py
+++ Lection1/lesson6UserInputWhileLoopBookExercises.py
@@ -15,3 +15,68 @@
 #     print(f"{question} is a multiple of 10!")
 # else:
 #     print(f"{question} is not a multiple of 10:(")
+
+#7-4
+# requested_pizza_toppings = []
+# while True:
+#     question = input("What would you like on you pizza? (Enter 'quit' to confirm order): ")
+#     if question == "quit":
+#         print(f"Here is your pizza with {requested_pizza_toppings}!")
+#         break
+#     else:
+#         requested_pizza_toppings.append(question)
+#         print(f"{question} has been added to your pizza!")
+
+#7-5
+# while True:
+#     question = int(input("Hello, what is your age? "))
+#     age = question
+#     if age < 3:
+#         print("Your ticket is free!")
+#     elif 3 <= age <= 12:
+#         print("Your ticket costs $10;)")
+#     else:
+#         print("Your ticket costs $15:)")
+
+#7-6
+#suck me
+
+#7-7
+# while True:
+#     print("lmao")
+
+#7-8 and 7-9
+# sandwich_orders = [
+#     "chicken and bacon sandwich",
+#     "pastrami sandwich",
+#     "eggsalad sandwich",
+#     "monte cristo sandwich",
+#     "pastrami sandwich",
+#     "meatball sandwich",
+#     "pastrami sandwich",
+#     "sloppy joe",
+# ]
+# finished_sandwich_orders = []
+# for sandwich in list(sandwich_orders):
+#     if sandwich == "pastrami sandwich":
+#         print(f"The deli has run out of {sandwich}s. order a new")
+#         sandwich_orders.remove(sandwich)
+#     else:
+#         print(f"Your {sandwich} was made, Enjoy!")
+#         finished_sandwich_orders.append(sandwich)
+#         sandwich_orders.remove(sandwich)
+# print(f"All of these sandwiches was made: {finished_sandwich_orders}")
+
+#7-10
+dream_vacations = []
+
+run_flag = True
+while run_flag:
+    question = input("Where is your dream vacation? ")
+    if question == "end poll":
+        run_flag = False
+    dream_vacations.append(question)
+print(f"The dream vacations of the poeple are: {dream_vacations}")
+
+
+
---
+++ Lection1/lesson7FunctionsBookAssignments.py
@@ -0,0 +1,119 @@
+#book assignments
+#8-1
+def display_message():
+    print("Hello, I have learned to write a function!")
+display_message()
+
+#8-2
+def favorite_book(title):
+    print(f"My favorite book is {title}")
+
+favorite_book("Djævlens lærling")
+
+#8-3 and 8-4
+def make_shirt(text = "I Love Python", size = "L"):
+    print(f"Here is a size {size} shirt with this text on the front: {text}")
+
+make_shirt()
+make_shirt(size="L")
+make_shirt(size="XXXXL", text="MISTER FAT MAAAAAN")
+
+#8-5
+def describe_city(name, country="Denmark"):
+    print(f"{name} is in {country}")
+
+describe_city("Aalborg")
+describe_city("Viborg")
+describe_city("Paris", country="France")
+
+#8-6
+def city_country(city: str, country: str):
+    formatted_message = f"{city.title()}, {country.title()}"
+    return formatted_message
+
+print(city_country("aalborg", "denmark"))
+print(city_country("viborg", "denmark"))
+print(city_country("tokyo", "japan"))
+
+#8-7
+def make_album(artist, album_title, number_of_songs=None):
+    if number_of_songs:
+        album = {
+            "artist": artist,
+            "album title": album_title,
+            "number of songs": number_of_songs
+        }
+    else:
+        album = {
+            "artist": artist,
+            "album title": album_title,
+        }
+    return album
+album1 = make_album("KSI", "All Over The Place", number_of_songs=20)
+print(album1)
+album2 = make_album("Post Malone", "Hollywood's Bleeding")
+print(album2)
+album3 = make_album("The Longest Johns", "Smoke and Oakum", number_of_songs=10)
+print(album3)
+
+#8-8
+# while True:
+#     print("I can compile your favorite singer and their album! (type 'quit' to exit program)")
+#     artist = input("What is the name of the artist? ")
+#     album_name = input("What is the name of their album? ")
+#     if artist == 'quit' or album_name == 'quit':
+#         break
+#     print(f" Here is the compilation: {make_album(artist, album_name)}")
+
+#8-9 and 8-10 and 8-11
+messages = [
+    "Hello",
+    "How",
+    "Are",
+    "you",
+    "doing?"
+]
+def show_messages(messages):
+    print(messages)
+
+show_messages(messages)
+
+def send_messages(messages):
+    new_messages = []
+    for message in messages:
+        new_messages.append(message)
+        print(message)
+    return new_messages
+new_list = send_messages(messages[:])
+print(messages)
+print(new_list)
+
+#8-12
+def sandwich(*args):
+    print("You have ordered a sandwich with the following:")
+    for arg in args:
+        print(f"- {arg}")
+
+sandwich("pepperoni", "ground beef", "salad", "Mushrooms", "pickles")
+sandwich("milk", "cheese", "gouda", "parmachese")
+sandwich("breed", "cum")
+
+#8-13
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+user_profile = build_profile('Nikolaj', 'Nim',
+ location='Aalborg',
+ field='AI', occupation="Choir singer")
+print(user_profile)
+
+#8-14
+def make_car(manufacturer, model, **kwargs):
+    kwargs["manufacturer"] = manufacturer
+    kwargs["model"] = model
+    return kwargs
+
+car = make_car("toyota", "Aygo", color="White", sky_window="Sky window")
+print(car)
