f7fe7b8fb493be3cbb4eb48b2c98bc8dfcbfc29c
Nikolaj Nim <nnjo22@student.aau.dk>
2023-10-17 15:11:40 +0200

    lesson and miniproject Progress

--- Lection1/Miniprojekt_2_robotnavigation/main.py
+++ Lection1/Miniprojekt_2_robotnavigation/main.py
@@ -1,24 +1,18 @@
 import random
 import pygame as pg
-import sys
-import os
 
 def draw_champ_n_goal(screen, cost_grid):
     red = (255, 0, 0)
     blue = (0, 0, 255)
-
-def restart():
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
 def draw_tiles(screen, cost_grid):
     width = screen.get_width()
     height = screen.get_height()
     box_size = (25, 25)
 
-    font = pg.font.Font(None, 35)
+    # font = pg.font.Font(None, 35)
 
-    for y in range(int(height/box_size[1])):
-        for x in range(int(width/box_size[0])):
+    for x in range(int(height/box_size[0])):
+        for y in range(int(width/box_size[1])):
             box_pos = (x * box_size[0], y * box_size[1])
 
             # text = font.render(f"{cost_grid[y][x]}", True, (0, 0, 0))
@@ -26,21 +20,24 @@ def draw_tiles(screen, cost_grid):
             # text_x = ((box_pos[0] + box_size[0] / 2) - text_w / 2)
             # text_y = ((box_pos[1] + box_size[1] / 2) - text_h / 2)
             # text_pos = (text_x, text_y)
-            # screen.blit(text, text_pos)2
-            if cost_grid[y][x] == 0:
+            # screen.blit(text, text_pos)
+            if cost_grid[x][y] == 0:
                 pg.draw.rect(screen, (0,177,0), (box_pos, box_size))
-            elif cost_grid[y][x] == 1:
+            elif cost_grid[x][y] == 1:
                 pg.draw.rect(screen, (0, 100, 255), (box_pos, box_size))
+            else:
+                pg.draw.rect(screen, (4,99,4), (box_pos, box_size))
             pg.draw.rect(screen, (0, 0, 0), (box_pos, box_size), 1)
+
 def cost_grid(screen):
     width = screen.get_width()
     height = screen.get_height()
     box_size = (25, 25)
     box_list = []
-    for y in range(int(height / box_size[1])):
+    for x in range(int(height / box_size[0])):
         box_list.append([])
-        for x in range(int(width / box_size[0])):
-            box_list[y].append(random.randint(0, 1))
+        for y in range(int(width / box_size[1])):
+            box_list[x].append(random.randint(0, 2))
     return box_list
 def main():
     pg.init()
@@ -62,9 +59,8 @@ def main():
                 main()
             if event.type == pg.QUIT or keys[pg.K_ESCAPE]:
                 run_flag = False
-
-
         pg.display.flip()
+
 if __name__ == "__main__":
     main()
 
