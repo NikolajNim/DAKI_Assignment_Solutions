6fac8850e7d1859f0ada2ab6d758de4956f3a381
Nikolaj Nim <nnjo22@student.aau.dk>
2023-11-23 00:21:26 +0100

    lesson and miniproject Progress DONE!!!!!

--- Mineproject_2_v2/a_star.py
+++ Mineproject_2_v2/a_star.py
@@ -1,43 +1,59 @@
 import heapq
 import random
-from queue import PriorityQueue
 import pygame as pg
+
+
 class Astar:
+    # The Astar class contains all the necessary methods for the A* algorithm.
     def __init__(self, matrix, matrix_size, box_size):
+        # These are the necessary parameters to initialize this class:
+        # A Matrix (List of lists), A Matrix size(columns and rows) and a box size when drawing with pygame.
         self.matrix = matrix
         self.matrix_size = matrix_size
         self.box_size = box_size
 
-
-    def color_picker(self, rand_num):
-        if rand_num == 1:
-            return (0, 177, 0)
-        elif rand_num == 2:
-            return (4, 99, 4)
-        elif rand_num == 5:
-            return (0, 100, 255)
-        else:
-            return (20, 20, 20)
-
-    def cost(self, point):
+    def cost(self, p):
+        # This method defines the cost of a specific value in the matrix.
         cost = [1, 2, 5, 1000]
-        return cost[self.matrix[point[0]][point[1]] - 1]
-    def draw_grid(self, screen):
+        return cost[self.matrix[p[0]][p[1]] - 1]
 
+    def color_picker(self, num):
+        # This method returns an RGB-Color Tuple depending on what number is the input.
+        if num == 1:
+            return (0, 177, 0)  # Green.
+        elif num == 2:
+            return (4, 99, 4)  # Darker green.
+        elif num == 5:
+            return (0, 100, 255)  # Blue.
+        else:
+            return (82, 68, 57)  # Muddy brown.
+
+    def draw_grid(self, screen):
+        # This method uses a nested loop to iterate over the given matrix
+        # and draw different colored squares and black outlines.
+        # The different colors are based of the random numbers in the matrix, and they are processed
+        # through the cost() and color_picker() metodes.
+        # The positions of the squares are based on the x, y indexes in the matrix scaled with the box_size variable.
         for x, col in enumerate(self.matrix):
             for y, row in enumerate(col):
-                pg.draw.rect(screen, self.color_picker(self.cost((x, y))), ((x * self.box_size[0], y * self.box_size[1]), self.box_size))
-                pg.draw.rect(screen, (0, 0, 0), ((x * self.box_size[0], y * self.box_size[1]), self.box_size), 1)
+                pg.draw.rect(screen, self.color_picker(self.cost((x, y))),
+                             ((x * self.box_size[0], y * self.box_size[1]), self.box_size))
+                pg.draw.rect(screen, (0, 0, 0),
+                             ((x * self.box_size[0], y * self.box_size[1]), self.box_size), 1)
 
     def draw_a_b(self, screen):
+        # This method draws the start point(a) and end point(b) and returns their respective matrix index positions.
         red = (255, 0, 0)
         orange = (255, 140, 0)
 
+        # The position of a is based on a random number between 0 and 31(current matrix size - 1)
         ax = random.randint(0, self.matrix_size[0] - 1)
         ay = random.randint(0, self.matrix_size[1] - 1)
         a_pos = (ax * self.box_size[0], ay * self.box_size[1])
         a_matrix_pos = (ax, ay)
 
+        # The position of b is based on the position of a.
+        # By subtracting the matrix size with a, the opposite position is the result.
         bx = self.matrix_size[0] - ax
         by = self.matrix_size[1] - ay
         b_pos = (bx * self.box_size[0] - self.box_size[0], by * self.box_size[1] - self.box_size[1])
@@ -48,23 +64,28 @@ class Astar:
 
         return a_matrix_pos, b_matrix_pos
 
-
     def neighbors(self, node):
-        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        # This metode returns a list of all neighbors to a specific point(node).
+        # Add these to directions for diagonal movement: [1, 1], [-1, -1], [-1, 1], [1, -1]
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         result = []
 
-        for dir in dirs:
-            neighbor = (node[0] + dir[0], node[1] + dir[1])
+        for direction in directions:
+            neighbor = (node[0] + direction[0], node[1] + direction[1])
+            # Only add the neighbor to the list, if it exists within the matrix.
             if 0 <= neighbor[0] < len(self.matrix) and 0 <= neighbor[1] < len(self.matrix[0]):
                 result.append(neighbor)
         return result
+
     def heuristic(self, a, b):
+        # This method calculates the distance between two point(a and b).
+        # It is used to help the algorithm prioritize what direction to choose.
         (ax, ay) = a
         (bx, by) = b
-
         return abs(ax - bx) + abs(ay - by)
 
     def astar_search(self, a, b):
+        # This method searches for point b starting from point a.
         frontier = [(0, a)]
         came_from = {}
         cost_so_far = {}
@@ -73,12 +94,9 @@ class Astar:
 
         while frontier:
             current_priority, current = heapq.heappop(frontier)
-            print(f"Current in astar_search: {current}")
-            #print(f"a in astar_search: {a}")
-            #print(f"b in astar_search: {b}")
-            #print(f"current in astar_search: {current}")
+
             if current == b:
-                print("Halløj")
+                # Ends the method early when b is found.
                 break
 
             for next_node in self.neighbors(current):
@@ -86,7 +104,6 @@ class Astar:
                 if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
                     cost_so_far[next_node] = new_cost
                     priority = new_cost + self.heuristic(b, next_node)
-                    #print(f"priority from astar_search: {priority}")
                     heapq.heappush(frontier, (priority, next_node))
                     came_from[next_node] = current
 
--- Mineproject_2_v2/main.py
+++ Mineproject_2_v2/main.py
@@ -1,9 +1,8 @@
-import random
 import numpy as np
 import pygame as pg
 import a_star
 
-def main():
+def initialize_astar():
     pg.init()
     screen = pg.display.set_mode((800, 800))
     white = (255, 255, 255)
@@ -12,50 +11,34 @@ def main():
     matrix_size = (32, 32)
     matrix = np.random.randint(1, 5, matrix_size)
 
-    # new_matrix = []
-    # for x in range(matrix_size[0]):
-    #     new_matrix.append([])
-    #     for y in range(matrix_size[1]):
-    #         if x % 2 == 0 and y % 2 == 0:
-    #             new_matrix[x].append(4)
-    #         else:
-    #             new_matrix[x].append(1)
-    # print(new_matrix)
-
-
-    print(matrix)
-
     astar = a_star.Astar(matrix, matrix_size, box_size=(25, 25))
     astar.draw_grid(screen)
     a, b = astar.draw_a_b(screen)
 
-    # for x, col in enumerate(matrix):
-    #     for y, output in enumerate(col):
-    #         print(x, y)
-
-    # print(a, b)
-
     came_from, cost_so_far = astar.astar_search(a, b)
-    print(a, b)
     path = astar.reconstruct(came_from, a, b)
-    print(path)
 
     scaled_path = [(x * 25, y * 25) for x, y in path]
     adjusted_path = [(x + 12.5, y + 12.5) for x, y in scaled_path]
 
     pg.draw.lines(screen, white, False, adjusted_path, 4)
 
+
+def main():
+    initialize_astar()
+
     run_flag = True
     while run_flag:
-
         for event in pg.event.get():
             keys = pg.key.get_pressed()
 
             if keys[pg.K_r]:
-                main()
+                initialize_astar()
             if event.type == pg.QUIT or keys[pg.K_ESCAPE]:
                 run_flag = False
         pg.display.flip()
+
+    pg.quit()
 
 
 if __name__ == "__main__":
